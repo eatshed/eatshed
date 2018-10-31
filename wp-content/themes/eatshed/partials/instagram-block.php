2adadca3bb8c4876c29d9549a1114b8073319a53
Calum <calum.baines@hotmail.co.uk>
2018-10-31 21:05:16 +0000

    sign up component and insta basic styling (git ignore on wp-config)

--- wp-content/themes/eatshed/partials/instagram-block.php
+++ wp-content/themes/eatshed/partials/instagram-block.php
@@ -1,5 +1,6 @@
-<div class="instagram-block">
-    <h2>If you don't already know</h2>
-    <h3>Follow us on instagram fellow plant pusher</h3>
-    Insta integration
-</div>
+<section class="instagram-block">
+    <div class="container">
+        <h2 class="instagram-block__title">If you don't already know</h2>
+        <h3 class="instagram-block__sub">Follow us on instagram fellow plant pusher</h3>
+    </div>
+</section>
--- wp-content/themes/eatshed/partials/signup-block.php
+++ wp-content/themes/eatshed/partials/signup-block.php
@@ -1,3 +1,7 @@
-<div class="signup-block">
-    <h3>Sign-up for our Forthnightly newsletter! </h3> <a class="btn btn--primary" href="">Signup</a> <-- Link to modal
-</div>
+<section class="signup-block">
+    <div class="container">
+        <div class="signup-block__container">
+            <h3 class="signup-block__title">Sign-up for our Forthnightly newsletter! </h3> <a class="btn btn--primary" href="">Signup</a>
+        </div>
+    </div>
+</section>
