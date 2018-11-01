6096cb476657ed4c46c5733b30d42bc7897f9e8a
Calum <calum.baines@hotmail.co.uk>
2018-11-01 22:41:02 +0000

    banner component added

--- wp-content/themes/eatshed/front-page.php
+++ wp-content/themes/eatshed/front-page.php
@@ -2,6 +2,7 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 <?php include 'partials/message-block.php'; ?>
+<?php include 'partials/banner.php'; ?>
 <?php include 'partials/carousel.php'; ?>
 <?php include 'partials/link-block.php'; ?>
 <?php include 'partials/content-block.php'; ?>
