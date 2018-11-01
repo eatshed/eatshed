5b95df6ff8676660b78f8598e373b48a24aca9d5
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-11-01 08:44:04 +0000

    Update blog nav

--- wp-content/themes/eatshed/partials/header-blog.php
+++ wp-content/themes/eatshed/partials/header-blog.php
@@ -3,8 +3,10 @@
 	get_template_part( 'partials/head' );
 ?>
 <header role="banner">
-	<div class="container">
-	    <a class="logo" href="<?php echo home_url(); ?>/blog"><img src="<?php echo get_template_directory_uri() ?>/images/logo-blog.svg" alt="Shed Logo" /></a>
-		<nav><?php wp_nav_menu( array('theme_location' => 'categories') ); ?></nav>
+	<div class="container is-fluid">
+		<nav class="header-blog"><?php wp_nav_menu( array('theme_location' => 'categories') ); ?></nav>
+		<div class="container">
+			<a class="logo" href="<?php echo home_url(); ?>/blog"><img src="<?php echo get_template_directory_uri() ?>/images/logo-blog.svg" alt="Shed Logo" /></a>
+		</div>
 	</div>
 </header><!-- /page-masthead -->
