a8928a40b7b29a9fc87b23475938eeed79688f9e
Calum <calum.baines@hotmail.co.uk>
2018-11-01 20:58:03 +0000

    button styles and font variables changed

--- wp-content/themes/eatshed/partials/content-block.php
+++ wp-content/themes/eatshed/partials/content-block.php
@@ -3,9 +3,9 @@
         <div class="column is-half">
             <img class="content-block__image" src="<?php the_field('content_image') ?>" alt="" />
         </div>
-        <div class="column is-half">
-            <h2><?php the_field('content_title');?>
-            <span><?php the_field('content_subtitle');?></span></h2>
+        <div class="column is-half is-offset-1">
+            <h2 class="content-block__title"><?php the_field('content_title');?>
+            <span class="content-block__sub"><?php the_field('content_subtitle');?></span></h2>
             <?php the_field('content_para');?>
             <a class="btn btn--primary" href="<?php the_field('content_link_url');?>"><?php the_field('content_link_title');?></a>
         </div>
