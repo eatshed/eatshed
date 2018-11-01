a504e3576361bd056a03dc000b56f84eec3772d8
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-11-01 10:20:01 +0000

    Add instagram feed

--- wp-content/themes/eatshed/partials/instagram-block.php
+++ wp-content/themes/eatshed/partials/instagram-block.php
@@ -2,5 +2,7 @@
     <div class="container">
         <h2 class="instagram-block__title">If you don't already know</h2>
         <h3 class="instagram-block__sub">Follow us on instagram fellow plant pusher</h3>
+
+        <?php echo do_shortcode("[instagram-feed]"); ?>
     </div>
 </section>
