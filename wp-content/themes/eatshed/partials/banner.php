aebd6d50b92bb66200f7df6d079502e2717998fb
Calum <calum.baines@hotmail.co.uk>
2018-11-01 23:01:57 +0000

    spotlight styles added to event template and include for compnents

--- wp-content/themes/eatshed/partials/banner.php
+++ wp-content/themes/eatshed/partials/banner.php
@@ -1,6 +1,6 @@
 <section class="banner">
     <div class="banner__container container">
-        <h1 class="banner__title">- Cutlery Works -</h1>
+        <h1 class="banner__title">- <?php the_title(); ?> -</h1>
         <p class="banner__subheading">upstairs at CW Established 2018, Shed is the healthy living lifestyle brand, centered around 100% plant-based foods.</p>
     </div>
 </section>
--- wp-content/themes/eatshed/templates/template-events.php
+++ wp-content/themes/eatshed/templates/template-events.php
@@ -4,22 +4,28 @@ the_post();
 get_header(); 
 ?>
 
-	<h1><?php the_title(); ?></h1> (BANNER)
-	
-
+<?php include 'partials/message-block.php'; ?>
+ <?php include 'partials/banner.php'; ?>  <!-- can I include these here? -->
+<div class="container">
     <h2>Upcoming Shed events</h2>
-
-    <?php
-        $the_query = new WP_Query(array( 'order_by'=>'date', 'order'=>'desc' , 'post_type'=>'events'));
-        while ( $the_query->have_posts() ) : $the_query->the_post();
-    ?>
-        <a class="COMPONENT-NAME" href="<?php the_permalink(); ?>" rel="bookmark">
-            <h3><?php the_title(); ?></h3>
-            <?php the_field('events_description'); ?>
-            <img src="<?php the_field('events_image'); ?>" alt="" />
-        </a>
-    <?php
-        endwhile;
-    ?>
+    <div class="columns is-multiline">
+        <?php
+            $the_query = new WP_Query(array( 'order_by'=>'date', 'order'=>'desc' , 'post_type'=>'events'));
+            while ( $the_query->have_posts() ) : $the_query->the_post();
+        ?>
+            <a class="spotlight column is-one-third" href="<?php the_permalink(); ?>" rel="bookmark">
+                <div class="spotlight__img">
+                    <img src="<?php the_field('events_image'); ?>" alt="" />
+                </div>
+                <div class="spotlight__text">
+                    <h3 class="spotlight__title"><?php the_title(); ?></h3>
+                    <?php the_field('events_description'); ?>
+                </div>
+            </a>
+        <?php
+            endwhile;
+        ?>
+    </div>
+</div>
 
 <?php get_footer(); ?>
