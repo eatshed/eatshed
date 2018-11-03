3112c2fcbe8b9ba6438367770caf6be89f35049d
Calum <calum.baines@hotmail.co.uk>
2018-11-03 11:23:55 +0000

    basic article styling

--- wp-content/themes/eatshed/partials/header-blog.php
+++ wp-content/themes/eatshed/partials/header-blog.php
@@ -3,16 +3,10 @@
 	get_template_part( 'partials/head' );
 ?>
 <header class="header header--blog">
-	<div class="container is-fluid is-flex">
-		<nav class=""><?php wp_nav_menu( array('theme_location' => 'categories') ); ?></nav>
+	<div class="container is-fluid is-flex u-flex-center">
+		<a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/nn-logo.svg" alt="NN Logo" /></a>
+		<nav class="header__nav u-flex-center"><?php wp_nav_menu( array('theme_location' => 'categories') ); ?></nav>
 		<a class="header__back" href="<?php echo home_url(); ?>">Back to eatshed.com</a>
 	</div>
 </header>
 
-<div class="container blog-logo">
-	<div class="columns is-mobile">
-		<div class="column is-half is-offset-one-quarter">
-			<a class="logo" href="<?php echo home_url(); ?>/blog"><img src="<?php echo get_template_directory_uri() ?>/images/logo-blog.svg" alt="Shed Logo" /></a>
-		</div>
-	</div>
-</div>
