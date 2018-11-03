fea4c660f0ce3f2342bd0a0d620d32741215b944
Calum <calum.baines@hotmail.co.uk>
2018-11-03 10:16:38 +0000

    map component and opening times

---
+++ wp-content/themes/eatshed/partials/map.php
@@ -0,0 +1,16 @@
+<section class="map">
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-10 is-offset-1">
+                <h2 class="u-text-orange map__title has-text-centered">Location</h2>
+                <p class="u-text-green map__location has-text-centered">Cutlery Works (Upstairs), 73-101 Neepsend Lane, Sheffield, S3 8AT</h2>
+            </div>
+            <div class="column is-10 is-offset-1 map__map">
+                Map widget
+            </div>
+            <div class="column is-10 is-offset-1 map__map u-flex-center">
+                <a class="btn btn--primary" href="">More venues</a>
+            </div>
+        </div>
+    </div>
+</section>
