6f836f7fadcbcea113504c4bba4dfc2291af3e41
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-11-04 12:07:51 +0000

    Update footer cols

--- wp-content/themes/eatshed/footer.php
+++ wp-content/themes/eatshed/footer.php
@@ -1,10 +1,10 @@
 <footer class="footer">
     <div class="container footer__container">
         <div class="columns">
-            <nav class="footer__nav column is-2">
+            <nav class="footer__nav column is-3">
                 <?php wp_nav_menu( array('theme_location' => 'footer') ); ?>
             </nav>
-            <div class="footer__logo column is-4 is-offset-2">
+            <div class="footer__logo column is-4 is-offset-1">
                 <img src="<?php echo get_template_directory_uri() ?>/images/logo-large.svg" alt="Shed Logo" />
             </div>
             <div class="footer__social column is-2 is-offset-2">
