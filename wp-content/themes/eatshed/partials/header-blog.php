6d3e4f2d44655fb1372657dbe236ff9f9189736a
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-11-04 21:12:33 +0000

    Button and message styles

--- wp-content/themes/eatshed/partials/header-blog.php
+++ wp-content/themes/eatshed/partials/header-blog.php
@@ -4,7 +4,7 @@
 ?>
 <header class="header header--blog">
 	<div class="container is-fluid is-flex u-flex-center">
-		<a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/nn-logo.svg" alt="NN Logo" /></a>
+		<a class="logo" href="<?php echo home_url(); ?>/blog"><img src="<?php echo get_template_directory_uri() ?>/images/nn-logo.svg" alt="NN Logo" /></a>
 		<nav class="header__nav u-flex-center"><?php wp_nav_menu( array('theme_location' => 'categories') ); ?></nav>
 		<a class="header__back" href="<?php echo home_url(); ?>">Back to eatshed.com</a>
 	</div>
