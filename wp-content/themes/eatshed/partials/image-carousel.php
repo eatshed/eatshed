9fcda8e8ee3826336b6ba3e52c6a1cbde896bd00
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-11-05 18:54:48 +0000

    Updating featured image

--- wp-content/themes/eatshed/partials/image-carousel.php
+++ wp-content/themes/eatshed/partials/image-carousel.php
@@ -1,5 +1,6 @@
 <section class="image-carousel container u-spacing-bottom">
     <div class="image-carousel__container">
+    <?php if ( 'true' == the_field('featured_post') ): ?>
         <div class="image-carousel__item" style="background-image: url(https://source.unsplash.com/random/1200x600);">
             <div class="image-carousel__content">
                 <p class="image-carousel__category has-text-centered">CATEGORY</p>
@@ -7,23 +8,23 @@
                 <p class="image-carousel__author has-text-centered">BY WRITERS NAME</p>
             </div>
         </div>
-        <div class="image-carousel__item" style="background-image: url(https://source.unsplash.com/random/1200x601);">
+        <div class="image-carousel__item" style="background-image: url(https://source.unsplash.com/random/1200x600);">
             <div class="image-carousel__content">
                 <p class="image-carousel__category has-text-centered">CATEGORY</p>
                 <h2 class="image-carousel__title has-text-centered">THE BEST PLANT-BASED PROBIOTIC FOODS TO KEEP YOUR GUT HAPPY</h2>
                 <p class="image-carousel__author has-text-centered">BY WRITERS NAME</p>
             </div>
         </div>
-        <div class="image-carousel__item" style="background-image: url(https://source.unsplash.com/random/1200x602);">
+        <div class="image-carousel__item" style="background-image: url(https://source.unsplash.com/random/1200x600);">
             <div class="image-carousel__content">
                 <p class="image-carousel__category has-text-centered">CATEGORY</p>
                 <h2 class="image-carousel__title has-text-centered">THE BEST PLANT-BASED PROBIOTIC FOODS TO KEEP YOUR GUT HAPPY</h2>
                 <p class="image-carousel__author has-text-centered">BY WRITERS NAME</p>
             </div>
         </div>
+    <?php endif; ?>
     </div>
 </section>
-
 <div class="carousel__nav">
     <img class="carousel__arrow carousel__arrow--next" src="<?php echo get_template_directory_uri() ?>/images/btn-prev.svg" alt="prev" />
     <img class="carousel__arrow carousel__arrow--prev" src="<?php echo get_template_directory_uri() ?>/images/btn-next.svg" alt="next" />
--- wp-content/themes/eatshed/partials/loop-post.php
+++ wp-content/themes/eatshed/partials/loop-post.php
@@ -8,34 +8,31 @@ query_posts( $args );
 ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-
-
-        <a class="blog-list__item spotlight column is-one-third" href="<?php the_permalink(); ?>" rel="bookmark">
-            <div class="spotlight__container">
-                <div class="spotlight__img">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail( '' );
-                    }
-                ?>
-                </div>
-                <div class="spotlight__text">
-                    <span class="spotlight__category">
-                        <?php foreach((get_the_category()) as $category)
-                            { echo $category->cat_name . '  '; }
-                        ?>
-                    </span>
-                    <h3 class="spotlight__title"><?php the_title(); ?></h3>
-                    <span class="spotlight__author">By <?php the_author(); ?> </span>
-
-                </div>
+    <a class="blog-list__item spotlight column is-one-third" href="<?php the_permalink(); ?>" rel="bookmark">
+        <div class="spotlight__container">
+            <div class="spotlight__img">
+            <?php
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail( '' );
+                }
+            ?>
             </div>
-        </a>
+            <div class="spotlight__text">
+                <span class="spotlight__category">
+                    <?php foreach((get_the_category()) as $category)
+                        { echo $category->cat_name . '  '; }
+                    ?>
+                </span>
+                <h3 class="spotlight__title"><?php the_title(); ?></h3>
+                <span class="spotlight__author">By <?php the_author(); ?> </span>
 
-        <?php the_posts_pagination( array(
-            'mid_size'  => 2,
-            'prev_text' => __( 'Prev', '' ),
-            'next_text' => __( 'Next', '' ),
-        ) ); ?>
-    <?php endwhile; ?>
+            </div>
+        </div>
+    </a>
+    <?php the_posts_pagination( array(
+        'mid_size'  => 2,
+        'prev_text' => __( 'Prev', '' ),
+        'next_text' => __( 'Next', '' ),
+    ) ); ?>
+<?php endwhile; ?>
     
