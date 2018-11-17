206532b9b64effc7826a0f3f18f62c61b48b313c
Calum <calum.baines@hotmail.co.uk>
2018-11-17 19:50:18 +0000

    footer tweaks

--- wp-content/themes/eatshed/footer.php
+++ wp-content/themes/eatshed/footer.php
@@ -4,10 +4,10 @@
             <nav class="footer__nav column is-3 is-half-mobile">
                 <?php wp_nav_menu( array('theme_location' => 'footer') ); ?>
             </nav>
-            <div class="footer__logo column is-4 is-half-mobile">
+            <div class="footer__logo column is-offset-1 is-4 is-half-mobile">
                 <img src="<?php echo get_template_directory_uri() ?>/images/logo-large.svg" alt="Shed Logo" />
             </div>
-            <div class="footer__social column is-2">
+            <div class="footer__social is-offset-1 column is-2">
                 <a href="<?php the_field('youtube', 'option'); ?>" class="footer__social-links"><img src="<?php echo get_template_directory_uri() ?>/images/icon-youtube.svg" alt="Youtube" /></a>
                 <a href="<?php the_field('instagram', 'option'); ?>" class="footer__social-links"><img src="<?php echo get_template_directory_uri() ?>/images/icon-insta.svg" alt="Instagram" /></a>
                 <a href="<?php the_field('facebook', 'option'); ?>" class="footer__social-links"><img src="<?php echo get_template_directory_uri() ?>/images/icon-facebook.svg" alt="Facebook" /></a>
