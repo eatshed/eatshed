4111649b9d21040f2c68dfa110fa18a43450b7d5
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-12-04 20:00:56 +0000

    Add a dark option to banner

--- wp-content/themes/eatshed/partials/banner-page.php
+++ wp-content/themes/eatshed/partials/banner-page.php
@@ -1,5 +1,5 @@
 <section class="banner" style="background-image: url(<?php the_field('banner_image');?>)">
-    <div class="banner__container container">
+    <div class="banner__container container <?php the_field('banner_text_colour');?>">
         <h1 class="banner__title">- <?php the_title(); ?> -</h1>
         <p class="banner__subheading"><?php the_field('banner_content');?></p>
     </div>
