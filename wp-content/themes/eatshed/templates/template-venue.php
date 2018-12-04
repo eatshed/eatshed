95444b09c18b816c752c0bdd3c764577d6de1f8f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-12-04 19:06:34 +0000

    Add the_content under banner of venue page.

--- wp-content/themes/eatshed/templates/template-venue.php
+++ wp-content/themes/eatshed/templates/template-venue.php
@@ -4,6 +4,11 @@ the_post();
 get_header(); 
 ?>
 <?php get_template_part( 'partials/banner-page'); ?>
+<div class="columns is-multiline">
+    <div class="column is-8 is-offset-2 u-center">
+        <?php the_content(); ?>
+    </div>
+</div>
 <?php get_template_part('partials/link-block-venue'); ?>
 <?php get_template_part('partials/opening-times'); ?>
 <?php get_template_part('partials/map'); ?>
