ce12eee910b9df775e25522ed191d1c4cf6cae7a
Stuart Chaffe <stu@stuartchaffe.co.uk>
2018-12-18 18:25:38 +0000

    Update Instagram content

--- wp-content/themes/eatshed/partials/instagram-block.php
+++ wp-content/themes/eatshed/partials/instagram-block.php
@@ -1,6 +1,6 @@
 <section class="instagram-block">
     <div class="container">
-        <h2 class="instagram-block__title">If you don't already know</h2>
+        <h2 class="instagram-block__title">If you don't already,</h2>
         <h3 class="instagram-block__sub">Follow us on instagram fellow plant pusher</h3>
     </div>
 </section>
