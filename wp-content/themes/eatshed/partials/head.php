e4460f1f3aa4a4b23393f96089a6e4678c9aab3c
Calum <calum.baines@hotmail.co.uk>
2018-12-24 08:01:18 +0000

    ga code added

--- wp-content/themes/eatshed/partials/head.php
+++ wp-content/themes/eatshed/partials/head.php
@@ -13,6 +13,18 @@
   crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/src/js/main.js"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111120106-1"></script>
+
+<!-- Analytics code -->
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-111120106-1');
+</script>
+<meta name="google-site-verification" content="o_wwCyg0ifKIptWahQPZD30t0A6AJIx51uSqUir9fTY" />
+
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
